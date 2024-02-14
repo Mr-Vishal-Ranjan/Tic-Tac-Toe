86aa5fd1a7c92e2985570283b21f058f55444e19
vishal <vr842002@gmail.com>
2024-02-14 18:01:55 +0530

    1.0.0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import { useState } from 'react';
 import './App.css';
+
+let count=1;
+
+function Square() {
+  const[value,setValue]=useState();
+
+  function handle(){
+    if(value==="X" || value==="O") return;
+
+    if(count%2===0){
+      setValue("X");
+    }else{
+      setValue("O");
+    }
+    count++;
+  }
+  return(
+    <div>
+      <button className='square' onClick={handle}>
+        {value}
+      </button>
+    </div>
+  );
+}
+
 
 export default function App(){
     return(
-      <div>
-        hello react
-      </div>
+      <>
+        <div className='urow'><Square/><Square/><Square/></div>
+          
+        <div className='mrow'><Square/><Square/><Square/></div>
+        
+        <div className='lrow'><Square/><Square/><Square/></div>
+      </>
     );
 };
